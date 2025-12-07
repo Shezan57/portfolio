a8f67389e4a167908df6b37c6c27c1d7986ec3c6
SHEZAN AHMED <shezanahamed57@gmail.com>
2025-12-07 15:19:49 +0800

    Add custom domain shezanai.tech

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Root path for Heroku
+  base: '/',  // Root path for custom domain
 })
